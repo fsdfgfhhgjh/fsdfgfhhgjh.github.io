a738d80ca7a0e9d06166052ea9f49b55f1109033
fsdfgfhhgjh <63265926+fsdfgfhhgjh@users.noreply.github.com>
2020-04-07 01:00:00 +0300

    Add files via upload

---
+++ precache-manifest.229adc7ccf9b7f34fc5358488375ca7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c64d430d0609986fd1c30c957a9c0b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5796c865f54d60d3b141",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "a6ddf426f88a152105d2",
+    "url": "/static/css/main.af2b6f64.chunk.css"
+  },
+  {
+    "revision": "5796c865f54d60d3b141",
+    "url": "/static/js/2.534d2a35.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.534d2a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ddf426f88a152105d2",
+    "url": "/static/js/main.64f02b14.chunk.js"
+  },
+  {
+    "revision": "9d4d9b170034a51ae547",
+    "url": "/static/js/runtime-main.3ace2857.js"
+  }
+]);
